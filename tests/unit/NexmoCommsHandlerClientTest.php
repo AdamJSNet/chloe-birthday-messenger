f02f604ff33f4e3c9983656f853cb9691dd8aa55
Adam Scholey <hello@adamjs.net>
2019-04-07 21:49:54 +0100

    Add tests for send voice method

--- tests/unit/NexmoCommsHandlerClientTest.php
+++ tests/unit/NexmoCommsHandlerClientTest.php
@@ -5,28 +5,31 @@ use App\Exceptions\CommsHandlerClientException;
 
 class NexmoCommsHandlerClientTest extends PHPUnit\Framework\TestCase
 {
-    public function test_it_throws_exception_when_client_send_fails()
+    public function test_it_throws_exception_when_client_sms_send_fails()
     {
         $messageClient = $this->getMockNexmoMessage()
             ->shouldReceive('send')
+            ->once()
             ->andThrow(new Exception('hello world'))
             ->mock();
 
         $nexmo = $this->getMockNexmoClient()
             ->shouldReceive('message')
+            ->once()
             ->andReturn($messageClient)
             ->mock();
 
         $this->expectExceptionObject(CommsHandlerClientException::operationFailed('send SMS', 'hello world'));
 
         $client = new NexmoCommsHandlerClient($nexmo);
-        $client->sendSms('to', 'from', 'message');
+        $client->sendSms('to', 'from', 'content');
     }
 
-    public function test_it_throws_exception_when_response_status_is_invalid()
+    public function test_it_throws_exception_when_sms_response_status_is_invalid()
     {
         $message = Mockery::mock(Nexmo\Message\Message::class)
             ->shouldReceive('getResponseData')
+            ->once()
             ->andReturn([
                 'messages' => [
                     [
@@ -38,24 +41,27 @@ class NexmoCommsHandlerClientTest extends PHPUnit\Framework\TestCase
 
         $messageClient = $this->getMockNexmoMessage()
             ->shouldReceive('send')
+            ->once()
             ->andReturn($message)
             ->mock();
 
         $nexmo = $this->getMockNexmoClient()
             ->shouldReceive('message')
+            ->once()
             ->andReturn($messageClient)
             ->mock();
 
         $this->expectExceptionObject(CommsHandlerClientException::operationFailed('send SMS', 'borked'));
 
         $client = new NexmoCommsHandlerClient($nexmo);
-        $client->sendSms('to', 'from', 'message');
+        $client->sendSms('to', 'from', 'content');
     }
 
-    public function test_it_sends_sms()
+    public function test_it_sends_sms_message()
     {
         $message = Mockery::mock(Nexmo\Message\Message::class)
             ->shouldReceive('getResponseData')
+            ->once()
             ->andReturn([
                 'messages' => [
                     [
@@ -67,16 +73,55 @@ class NexmoCommsHandlerClientTest extends PHPUnit\Framework\TestCase
 
         $messageClient = $this->getMockNexmoMessage()
             ->shouldReceive('send')
+            ->once()
             ->andReturn($message)
             ->mock();
 
         $nexmo = $this->getMockNexmoClient()
             ->shouldReceive('message')
+            ->once()
             ->andReturn($messageClient)
             ->mock();
 
         $client = new NexmoCommsHandlerClient($nexmo);
-        $this->assertTrue($client->sendSms('to', 'from', 'message'));
+        $this->assertTrue($client->sendSms('to', 'from', 'content'));
+    }
+
+    public function test_it_throws_exception_when_send_voice_message_fails()
+    {
+        $callsCollection = $this->getMockNexmoCalls()
+            ->shouldReceive('create')
+            ->once()
+            ->andThrow(new Exception('hello world'))
+            ->mock();
+
+        $nexmo = $this->getMockNexmoClient()
+            ->shouldReceive('calls')
+            ->once()
+            ->andReturn($callsCollection)
+            ->mock();
+
+        $this->expectExceptionObject(CommsHandlerClientException::operationFailed('send Voice', 'hello world'));
+
+        $client = new NexmoCommsHandlerClient($nexmo);
+        $this->assertTrue($client->sendVoice('to', 'from', 'content'));
+    }
+
+    public function test_it_sends_voice_message()
+    {
+        $callsCollection = $this->getMockNexmoCalls()
+            ->shouldReceive('create')
+            ->once()
+            ->mock();
+
+        $nexmo = $this->getMockNexmoClient()
+            ->shouldReceive('calls')
+            ->once()
+            ->andReturn($callsCollection)
+            ->mock();
+
+        $client = new NexmoCommsHandlerClient($nexmo);
+        $this->assertTrue($client->sendVoice('to', 'from', 'content'));
     }
 
     protected function getMockNexmoClient()
@@ -87,6 +132,11 @@ class NexmoCommsHandlerClientTest extends PHPUnit\Framework\TestCase
     protected function getMockNexmoMessage()
     {
         return Mockery::mock(Nexmo\Message\Client::class);
+    }
+
+    protected function getMockNexmoCalls()
+    {
+        return Mockery::mock(Nexmo\Call\Collection::class);
     }
 
     protected function tearDown()
