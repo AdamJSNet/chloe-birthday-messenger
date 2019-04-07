51aa6c31087f9c4560307bd53da6f8a41ca5145c
Adam Scholey <hello@adamjs.net>
2019-04-07 09:07:45 +0100

    Add use statements to script

--- script.php
+++ script.php
@@ -1,28 +1,33 @@
 <?php
 
+use App\Clients\LocalDataStoreClient;
+use App\Collections\MessageCollection;
+use App\Exceptions\MessageCollectionException;
+use App\Functions;
+
 require_once("src/bootstrap.php");
 
 try {
     $console->info("Running script...");
 
     // instantiate data store from local file system
-    $store = (new App\Clients\LocalDataStoreClient(
+    $store = (new LocalDataStoreClient(
         new SplFileObject(ROOT_DIR . "/" . getenv("MESSAGES_FILENAME"), "r+")
     ))->load();
 
     // create collection of all messages
-    $messageCollection = App\Collections\MessageCollection::fromArray($store->getData());
+    $messageCollection = MessageCollection::fromArray($store->getData());
 
     // retrieve messages to be processed in this iteration only
     $messages = $messageCollection->elapsed()->notSent();
 
     // summarise
     $count = $messages->count();
-    $info = "$count " . App\Functions::pluralise($count, "message", "messages") . " to process";
+    $info = "$count " . Functions::pluralise($count, "message", "messages") . " to process";
     $console->info($info);
 
     if ($count === 0) {
-        App\Functions::end($console);
+        Functions::end($console);
     }
 
     $success = [];
@@ -37,7 +42,7 @@ try {
         try {
             $messages->update($message);
             $success[] = $message;
-        } catch (App\Exceptions\MessageCollectionException $e) {
+        } catch (MessageCollectionException $e) {
             $fail[] = $message;
         }
     }
@@ -46,7 +51,7 @@ try {
 
     $countSuccess = count($success);
     $countFail = count($fail);
-    $info = "$countSuccess " . App\Functions::pluralise($countSuccess, "message", "messages")
+    $info = "$countSuccess " . Functions::pluralise($countSuccess, "message", "messages")
         . " processed successfully. $countFail failed.";
     $console->info($info);
 } catch (Exception $e) {
