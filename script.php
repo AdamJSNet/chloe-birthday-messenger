47596f3c9276fba490392c94cbbabdda058f8be0
Adam Scholey <hello@adamjs.net>
2019-04-08 20:25:48 +0100

    Replace message logic in script with message service class

--- script.php
+++ script.php
@@ -2,24 +2,25 @@
 
 use App\Clients\LocalDataStoreClient;
 use App\Clients\NexmoCommsHandlerClient;
-use App\Collections\MessageCollection;
-use App\Enum\MessageType;
-use App\Exceptions\CommsHandlerClientException;
 use App\Exceptions\MessageCollectionException;
+use App\Exceptions\MessageServiceException;
 use App\Functions;
+use App\Services\MessageService;
 
 require_once("src/bootstrap.php");
 
 try {
     $console->info("Running script...");
 
-    // instantiate data store from local file system
+    // In the absence of an IoC container, create our clients manually
+    $nexmo = new NexmoCommsHandlerClient(Functions::getNexmoClient());
+    $messageService = new MessageService($nexmo);
+
+    // Get all messages
     $store = (new LocalDataStoreClient(
         new SplFileObject(ROOT_DIR . "/" . getenv("MESSAGES_FILENAME"), "r+")
     ))->load();
-
-    // create collection of all messages
-    $messageCollection = MessageCollection::fromArray($store->getData());
+    $messageCollection = $messageService->getAllMessagesFromDataStore($store);
 
     // retrieve messages to be processed in this iteration only
     $messages = $messageCollection->elapsed()->notSent();
@@ -36,26 +37,11 @@ try {
     $success = [];
     $fail = [];
 
-    // instantiate our comms client
-    $nexmo = new NexmoCommsHandlerClient(Functions::getNexmoClient());
-    $senderSms = getenv("NEXMO_SMS_SENDER");
-    $senderVoice = getenv("NEXMO_VOICE_SENDER");
-
     /** @var App\Contracts\MessageInterface $message */
     foreach ($messages as $message) {
         try {
-            switch ($message->getType()) {
-                case MessageType::TYPE_SMS:
-                    $nexmo->sendSms($message->getRecipient(), $senderSms, $message->getContent());
-                    break;
-                case MessageType::TYPE_VOICE:
-                    $nexmo->sendVoice($message->getRecipient(), $senderVoice, $message->getContent());
-                    break;
-                default:
-                    throw new Exception("Unrecognised Message Type");
-                    break;
-            }
-        } catch (CommsHandlerClientException $e) {
+            $messageService->sendMessage($message);
+        } catch (MessageServiceException $e) {
             $console->error($e->getMessage());
             $fail[] = $message->getId();
             continue;
